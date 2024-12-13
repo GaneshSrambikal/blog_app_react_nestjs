0a25e958a0f7e7a32640f503848be375d2c03980
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2024-12-14 03:29:50 +0530

    update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5019;
   const app = await NestFactory.create(AppModule, { cors: true });
 
   // Enable global validation pipe
